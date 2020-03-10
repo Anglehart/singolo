2ec50a9889bb617086f4181e5e4ec59c94d295ae
Boris Kobzarev <planez@bk.ru>
2020-03-11 00:15:31 +0300

    feat: add active menu

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const IMAGES = document.getElementById('portf-images');
+const MENU = document.getElementById('menu');
 
 IMAGES.addEventListener('click', (event) => {
   IMAGES.querySelectorAll('img').forEach(el => el.classList.remove('image-active'));
   event.target.classList.add('image-active');
+})
+
+MENU.addEventListener('click', (event) => {
+  MENU.querySelectorAll('a').forEach(el => el.classList.remove('menu-active'));
+  event.target.classList.add('menu-active');
 })
