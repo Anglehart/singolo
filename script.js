c77090a4d4157215b04bc08021844ff1012dad1c
Boris Kobzarev <planez@bk.ru>
2020-03-14 10:53:48 +0300

    feat: add on/off phone screens

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const IMAGES = document.getElementById('portf-images');
 const MENU = document.getElementById('menu');
+const PHONEV = document.getElementById('phone-content-v');
+const PHONEH = document.getElementById('phone-content-h');
+let vTrigger = true;
+let hTrigger = true;
 
 IMAGES.addEventListener('click', (event) => {
   IMAGES.querySelectorAll('img').forEach(el => el.classList.remove('image-active'));
@@ -11,4 +15,24 @@ MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll('a').forEach(el => el.classList.remove('menu-active'));
   event.target.classList.add('menu-active');
   MENU.querySelectorAll('li').forEach(el => el.classList.remove('menu-active'));
+})
+
+document.getElementById('iphone-v').addEventListener('click', (event) => {
+  if (vTrigger == true){
+    PHONEV.classList.add('hidden');
+    vTrigger = false;
+  } else {
+    PHONEV.classList.remove('hidden');
+    vTrigger = true;
+  }
+})
+
+document.getElementById('iphone-h').addEventListener('click', (event) => {
+  if (hTrigger == true){
+    PHONEH.classList.add('hidden');
+    hTrigger = false;
+  } else {
+    PHONEH.classList.remove('hidden');
+    hTrigger = true;
+  }
 })
